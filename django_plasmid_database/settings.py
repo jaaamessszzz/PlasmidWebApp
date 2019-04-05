493efa938d6a57851da2b930f4b6eb531162d958
Jared Lumpe <mjlumpe@gmail.com>
2019-04-05 14:35:27 -0700

    Get database connection config from environment vars

--- django_plasmid_database/settings.py
+++ django_plasmid_database/settings.py
@@ -82,11 +82,11 @@ AUTH_USER_MODEL = 'plasmid_database.User'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'plasmid_database',
-        'USER': 'jaaamessszzz',  # todo: use environment variable to populate
-        'PASSWORD': 'herpderp~',
-        'HOST': 'localhost',
-        'PORT': '',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
     }
 }
 
