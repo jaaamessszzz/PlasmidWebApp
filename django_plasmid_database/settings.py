222da240f529ed9ac601f67645a69257234c4a57
jaaamessszzz <james.lucas@berkeley.edu>
2020-04-06 22:08:50 -0700

    STATIC_ROOT?

--- django_plasmid_database/settings.py
+++ django_plasmid_database/settings.py
@@ -140,6 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "plasmid_database", "static")
+
 
 # --- Mozilla Developer Things --- #
 
