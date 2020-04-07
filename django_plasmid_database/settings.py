1257733cda20d6c8a7af4925380f28123b720a5c
jaaamessszzz <james.lucas@berkeley.edu>
2020-04-06 19:14:55 -0700

    database setings

--- django_plasmid_database/settings.py
+++ django_plasmid_database/settings.py
@@ -79,16 +79,28 @@ AUTH_USER_MODEL = 'plasmid_database.User'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'plasmid_database',
-        'USER': 'jaaamessszzz',  # todo: use environment variable to populate
-        'PASSWORD': 'herpderp~',
-        'HOST': 'localhost',
-        'PORT': '',
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'plasmid_database',
+            'USER': 'jaaamessszzz',  # todo: use environment variable to populate
+            'PASSWORD': 'herpderp~',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
