ee6f756088b5ee46a5cdfbe9cb598801d3a5e63d
jaaamessszzz <james.lucas@berkeley.edu>
2020-04-08 18:11:42 -0700

    Updated settings to make it easier to deploy through AWS

--- django_plasmid_database/settings.py
+++ django_plasmid_database/settings.py
@@ -79,16 +79,28 @@ AUTH_USER_MODEL = 'plasmid_database.User'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'plasmid_database',
-        'USER': 'jaaamessszzz',  # todo: use environment variable to populate
-        'PASSWORD': 'herpderp~',
-        'HOST': 'localhost',
-        'PORT': '',
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'plasmid_database',
+            'USER': 'jaaamessszzz',  # todo: use environment variable to populate
+            'PASSWORD': 'herpderp~',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
@@ -128,6 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "plasmid_database", "static")
+
 
 # --- Mozilla Developer Things --- #
 
