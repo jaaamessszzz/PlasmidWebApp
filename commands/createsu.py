fc78dfbb948188f889127d29c07fcb6479cf2bde
jaaamessszzz <james.lucas@berkeley.edu>
2020-04-09 21:37:55 -0700

    added manage commands

---
+++ commands/createsu.py
@@ -0,0 +1,14 @@
+"""
+Create a default admin account
+
+Source:
+https://realpython.com/deploying-a-django-app-and-postgresql-to-aws-elastic-beanstalk/#configure-eb-initialize-your-app
+"""
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser("Admin", "admin@admin.com", "plasmid")
