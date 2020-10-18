c3ed13b5e55ad3569d10095dbb35dc1d7f98d356
jaaamessszzz <james.lucas@berkeley.edu>
2020-10-18 01:28:34 -0700

    Added .ebextensions to main with updated paths Added createsu.py

---
+++ plasmiddb/management/commands/createsu.py
@@ -0,0 +1,13 @@
+"""
+Create a default admin account
+Source:
+https://realpython.com/deploying-a-django-app-and-postgresql-to-aws-elastic-beanstalk/#configure-eb-initialize-your-app
+"""
+from django.core.management.base import BaseCommand
+from plasmid_database.models import User
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username="Admin").exists():
+            User.objects.create_superuser("Admin", "admin@admin.com", "plasmid")
